25ae63763842c98b255fb56f52367e859336beed
jcaple <capable2@gmail.com>
2017-05-22 18:51:00 -0400

    Working on integrating Awards Table

--- setup/CreateDB.sql
+++ setup/CreateDB.sql
@@ -19,6 +19,17 @@ create table Users (
 	role VARCHAR
 );
 
+create table Awards (
+	userId integer PRIMARY KEY REFERENCES Users(ID),
+	nominatorId integer REFERENCES Users(ID) NOT NULL,
+	rating integer NOT NULL,
+	comment VARCHAR NOT NULL,
+	nominateDate TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+	nominationUpdateDate TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+	nominationReviewData TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+	disposition VARCHAR
+);
+
 DROP USER IF EXISTS awards;
 CREATE USER awards WITH PASSWORD 'awards';
 grant all privileges on database devtech_awards to awards;
